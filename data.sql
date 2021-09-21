a12f76e9b9e18f3dd5738129fd3e704bb856ffa3
Leonardo Dias <leonardodias@ucl.br>
2021-09-21 14:38:00 -0300

    Create and add data.sql script

---
+++ data.sql
@@ -0,0 +1,6 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '02-03-2020', 0, true, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, true, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-03-12', 5, true, 8);
