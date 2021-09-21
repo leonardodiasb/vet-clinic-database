e26d801fcf457a365b21dd4ff832dfd83923aacc
Leonardo Dias <leonardodias@ucl.br>
2021-09-21 14:37:34 -0300

    Create and add schema.sql script

---
+++ schema.sql
@@ -0,0 +1,12 @@
+/* Database schema to keep the structure of entire database. */
+
+createdb vet_clinic
+
+CREATE TABLE animals (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50), date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL(10, 2),
+  PRIMARY KEY(id)
+);
