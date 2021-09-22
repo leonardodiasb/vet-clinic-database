ab798566e9a035fa2ff511b534bf2aad68d06f57
Leonardo Dias <leonardodias@ucl.br>
2021-09-22 14:59:21 -0300

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -12,3 +12,10 @@ VALUES ('Charmander', '2020-02-08', 0, false, -11),
 ('Angemon', '2005-06-12', 1, true, -45),
 ('Boarmon', '2005-06-07', 7, true, 20.4),
 ('Blossom', '1998-10-13', 3, true, 17);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
