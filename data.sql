03a58875ae0914ba08d0fd55b5f967cfca9edbee
Leonardo Dias <leonardodias@ucl.br>
2021-09-22 16:25:14 -0300

    Modify animals to include owners_id

--- data.sql
+++ data.sql
@@ -25,3 +25,9 @@ VALUES ('Pokemon'),
 ('Digimon');
 
 UPDATE animals SET species_id = CASE WHEN name like '%mon' THEN 2 ELSE 1 END;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
